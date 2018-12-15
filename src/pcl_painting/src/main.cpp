4c4d0f5eee0e6ef8975030aeb316f2bddcc02a82
Pedro Martins <martinspedro@ua.pt>
2018-12-15 17:15:18 +0000

    Working version. Lots of junk code
    No optimized. LIDAR point cloud sometimes miss colored points

--- src/pcl_painting/src/main.cpp
+++ src/pcl_painting/src/main.cpp
@@ -1,10 +1,10 @@
 #include <ros/ros.h>
 
 // PCL specific includes
-#include <pcl_ros/point_cloud.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/point_cloud.h>
 #include <sensor_msgs/PointCloud2.h>
 
 ros::Publisher pub;
@@ -13,46 +13,72 @@ typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 typedef pcl::PointXYZRGB PointC;
 typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudC;
 
+//using namespace pcl;
+
+void color_point_cloud(pcl::PointCloud<pcl::PointXYZRGB> cloud, uint8_t r, uint8_t g, uint8_t b)
+{
+    for (int i = 0; i < cloud.points.size(); i++) {
+        cloud.points[i].r = r;
+        cloud.points[i].g = g;
+        cloud.points[i].b = b;
+    }
+}
+
 void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
-    /**
-     * Create a colored point cloud object
-     */
-    PointCloudC cloud_colored;
-    pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
-    pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
 
-    pcl_conversions::toPCL(*cloud_msg, *cloud);
+    pcl::PointCloud<pcl::PointXYZRGB> cloud_colored;
+
+    //PointCloudC cloud_colored;
+    //pcl::PCLPointCloud2* cloud; // = new pcl::PCLPointCloud2;
+    //pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
+    
+    pcl::fromROSMsg(*cloud_msg, cloud_colored);
+   
+    // pack r/g/b into rgb
+    uint8_t r = 255, g = 0, b = 0;    // Example: Red color
+    //uint32_t rgb = ((uint32_t)r << 16 | (uint32_t)g << 8 | (uint32_t)b);
+     
+    //pcl::PointCloud<pcl::PointXYZ> cloud_xyz;
+    //pcl::PointCloud<pcl::PointXYZRGB> cloud_xyzrgb;
+    
+    //pcl::copyPointCloud(cloud, cloud_colored);
+    
+    color_point_cloud(cloud_colored, r, g, b);
+/*
+    for (size_t i = 0; i < cloud_colored.points.size(); i++) {
+        cloud_colored.points[i].r = r;
+        cloud_colored.points[i].g = g;
+        cloud_colored.points[i].b = b;
+    }
     //ROS_INFO("Got point cloud with %ld points", cloud->size());
-   //cloud->size; 
-   // c//loud_colored[0].rgb;
+    //cloud->size;
+    // c//loud_colored[0].rgb;
     //PointCloudC::Ptr colored_cloud(new PointCloudC());
 
     // Create a container for the data.
     //sensor_msgs::PointCloud2 output;
 
     // Container for original & filtered data
-    //pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
+    //pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2;
     //pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
     //pcl::PCLPointCloud2 cloud_filtered;
-           
+
     // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
     //pcl::PointCloud<pcl::PointXYZ> cloud;
     //pcl::fromROSMsg(*input, cloud);
- 
+
     //pcl::PointCloud<pcl::PointXYZRGB> cloud_colored;
     //pcl::copyPointCloud(cloud, cloud_colored);
-   // PointCloud<PointXYZ> cloud_xyz;
-   
-    //PointCloud<PointXYZRGB> cloud_xyzrgb;
-    //copyPointCloud(cloud, cloud_colored);
-    //cloud_colored.points.resize(cloud->points.size());
-    uint8_t r = 255, g = 0, b = 0;    //  Red color
-    uint32_t rgb = ((uint32_t)r << 16 | (uint32_t)g << 8 | (uint32_t)b);
-    cloud_colored.width = cloud->width;
-    cloud_colored.height = cloud-> height;
+    // PointCloud<PointXYZ> cloud_xyz;
 
-    cloud_colored.data = *cloud.points.data
+    //PointCloud<PointXYZRGB> cloud_xyzrgb;
+    //copyPointCloud(&cloud, &cloud_colored);
+
+    //cloud_colored.width = cloud->width;
+    //cloud_colored.height = cloud-> height;
+
+    //cloud_colored.data = cloud.points.data
     /*
     for (size_t i = 0; i < 1; i++) {
         cloud_colored[i].x = cloud[i].data[0];
@@ -67,7 +93,7 @@ void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
     // Convert to ROS data type
     //pcl_conversions::fromPCL(input, output);
     //pcl_conversions::fromPCL(cloud_colored, output);
-    
+
     // Publish the data.
     pub.publish(cloud_colored);
 }
@@ -83,8 +109,8 @@ int main(int argc, char** argv)
 
     // Create a ROS publisher for the output point cloud
     //pub = nh.advertise<sensor_msgs::PointCloud2>("output", 1);
-    pub = nh.advertise<pcl::PointCloud<pcl::PointXYZRGB> > ("colored_point_cloud", 1);
-    
+    pub = nh.advertise<pcl::PointCloud<pcl::PointXYZRGB> >("colored_point_cloud", 1);
+
     // Spin
     ros::spin();
 }
