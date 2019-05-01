598ff37d98766a01a8f16aabdb3c0811a243142e
Pedro Martins <martinspedro@ua.pt>
2019-05-01 20:37:49 +0100

    Update Image Visualizer

--- src/automatic_calibration/include/automatic_calibration/image_visualizer.hpp
+++ src/automatic_calibration/include/automatic_calibration/image_visualizer.hpp
@@ -36,10 +36,6 @@ public:
       return;
     }
 
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
-
     // Update GUI Window
     cv::imshow(OPENCV_WINDOW, cv_ptr->image);
     cv::waitKey(3);
