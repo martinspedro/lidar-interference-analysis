fc3afcbcf4c745b6a9304d1c8a4c25c64da33321
Pedro Martins <martinspedro@ua.pt>
2019-05-07 20:35:22 +0100

    Improved code readibility

--- src/automatic_calibration/src/point_cloud_visualizer.cpp
+++ src/automatic_calibration/src/point_cloud_visualizer.cpp
@@ -1,31 +1,22 @@
 #include "automatic_calibration/automatic_calibration.hpp"
 
+// Create viewer object
 pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
 
 
 void callback(const sensor_msgs::PointCloud2ConstPtr& point_cloud_msg) {
+    // create Point Cloud with Intensity Field
+    pcl::PointCloud<pcl::PointXYZI> point_cloud;
 
-//pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud;
-  //pcl::PointCloud<pcl::PointXYZ> cloud;
-//pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-  // Convert ROS message to colored point cloud object
-  //pcl::fromROSMsg(*point_cloud_msg, &cloud);
-  //pcl::PointCloud<pcl::PointXYZRGB>::Ptr ptrCloud(&cloud);
-    pcl::PointCloud<pcl::PointXYZI> cloud;
-    pcl::fromROSMsg(*point_cloud_msg, cloud);
+    // Convert ROS msg to Point Cloud
+    pcl::fromROSMsg(*point_cloud_msg, point_cloud);
+
+    // Create dynamic pointer to point cloud data
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloudPtr(new pcl::PointCloud<pcl::PointXYZI>);
-    *cloudPtr = cloud;
-/*
-      pcl::PCLPointCloud2 pcl_pc2;
-      pcl_conversions::toPCL(*point_cloud_msg,pcl_pc2);
-      pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-      pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
+    *cloudPtr = point_cloud;
 
-*/
-  //ROS_INFO("Got point cloud with %ld points", cloud->size());
-  //pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloudPtr(&cloud);
-
-  viewer.showCloud(cloudPtr,"Udacity");
+    // Add new data to viewer
+    viewer.showCloud(cloudPtr);
 }
 
 
@@ -33,14 +24,11 @@ int main(int argc, char** argv)
 {
   //Initialize ROS
   ros::init(argc, argv, "point_cloud_visualizer");
-  ros::NodeHandle nh;
 
+  ros::NodeHandle nh;
 
   // Ros subscriber for input point cloud2
   ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2>("velodyne_points", 1, callback);
-
-  // ROS publisher
-  //pub = nh.advertise<sensor_msgs::PointCloud2>("output_point_cloud", 1);
 
   ros::spin();
 
