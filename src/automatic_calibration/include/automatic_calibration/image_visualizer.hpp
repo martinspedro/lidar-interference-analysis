30657c2d0213e6bf01604d76c53211d79e759fd1
Pedro Martins <martinspedro@ua.pt>
2019-05-10 15:55:08 +0100

    Update image visualizer class

--- src/automatic_calibration/include/automatic_calibration/image_visualizer.hpp
+++ src/automatic_calibration/include/automatic_calibration/image_visualizer.hpp
@@ -1,16 +1,24 @@
+/**
+ * @file   image_visualizer.hpp
+ * @brief  Image Stream and Visualizer Header File
+ *
+ * @author Pedro Martins
+ * @date   @today
+ */
+
+
 static const std::string OPENCV_WINDOW = "Simple Video Stream Viewer";
 
-class ImageVisualizer
-{
+
+class ImageVisualizer {
   ros::NodeHandle nh_;
+
   image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
+  image_transport::Subscriber     image_sub_;
+  image_transport::Publisher      image_pub_;
 
 public:
-  ImageVisualizer()
-    : it_(nh_)
-  {
+  ImageVisualizer() : it_(nh_) {
     // Subscribe to input video feed and publish output video feed
     image_sub_ = it_.subscribe("/camera/image_color/raw", 1, &ImageVisualizer::imageCb, this);
     image_pub_ = it_.advertise("/camera/image_info", 1);
@@ -18,24 +26,21 @@ public:
     cv::namedWindow(OPENCV_WINDOW, 0);
   }
 
-  ~ImageVisualizer()
-  {
+  ~ImageVisualizer() {
     cv::destroyWindow(OPENCV_WINDOW);
   }
 
   /** @brief Callback function for image visualization
-   *   
+   *
    */
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
     cv_bridge::CvImagePtr cv_ptr;
 
-    try
-    {
+    try {
       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     }
-    catch (cv_bridge::Exception& e)
-    {
+    catch (cv_bridge::Exception& e) {
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
@@ -45,6 +50,6 @@ public:
     cv::waitKey(3);
 
     // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
+    //image_pub_.publish(cv_ptr->toImageMsg());
   }
 };
