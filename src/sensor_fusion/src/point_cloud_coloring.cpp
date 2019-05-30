da44e38e7f6fad750bbdcb326a0aee0aa5dd3eca
Pedro Martins <martinspedro@ua.pt>
2019-05-30 15:55:49 +0100

    Simplified launcher file. Sync callback and point coloring with static color

--- src/sensor_fusion/src/point_cloud_coloring.cpp
+++ src/sensor_fusion/src/point_cloud_coloring.cpp
@@ -37,7 +37,7 @@
 using namespace sensor_msgs;
 using namespace message_filters;
 
-pcl::visualization::CloudViewer viewer(VIEWER_NAME); //!< Create visualization object
+//pcl::visualization::CloudViewer viewer(VIEWER_NAME); //!< Create visualization object
 
 geometry_msgs::TransformStamped transformStamped; //!< Create geometric transform object
 
@@ -102,7 +102,7 @@ void callback_pcl(const sensor_msgs::PointCloud2::ConstPtr& point_cloud_msg) {
 
 
     // Add new data to viewer
-    viewer.showCloud(cloudPtr);
+    //viewer.showCloud(cloudPtr);
 
     // Publish the data.
     pub.publish(cloud_out);
@@ -125,10 +125,57 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
   }
 }
 
-void callback(const ImageConstPtr& image, const CameraInfoConstPtr& cam_info, const sensor_msgs::PointCloud2::ConstPtr& point_cloud_msg)
-{
-  ROS_INFO("I'm here!");
+/** @brief Callback to process imagem stream
+ *
+ * @param[in] image Pointer to Image data messages from ROS nodes
+ * @param[in] cam_info Information of camera calibration and frame characteristics
+ * @param[in] point_cloud_msg PointCloud2 messages from ROS nodes
+*/
+void callback(const ImageConstPtr& image,
+              const CameraInfoConstPtr& cam_info,
+              const sensor_msgs::PointCloud2::ConstPtr& point_cloud_msg) {
+  ROS_INFO("Data Sync successfull!");
+  // create Point Cloud with Intensity Field
 
+  PointCloudRGB point_cloud;
+  PointCloudRGB point_cloud_camera;
+
+  PointCloudRGB::Ptr cloudPtr(new PointCloudRGB);
+  PointCloudRGB::Ptr cloudCameraPtr(new PointCloudRGB);
+
+  sensor_msgs::PointCloud2 cloud_out, cloud_colored;
+
+  // Select RGB color
+  color_t point_cloud_RGB;
+  point_cloud_RGB.r = 255;
+  point_cloud_RGB.g = 123;
+  point_cloud_RGB.b = 10;
+
+
+  // Convert ROS msg to Point Cloud
+  fromROSMsg(*point_cloud_msg, point_cloud);
+
+  // Initialize pointer to point cloud data
+  *cloudPtr = point_cloud;
+
+  // Perform color manipulation on Point Cloud
+  color_point_cloud(cloudPtr, point_cloud_RGB);
+
+  pcl::toROSMsg(*cloudPtr,cloud_colored );
+
+  try {
+      tf2::doTransform(cloud_colored, cloud_out, transformStamped);
+  }
+  catch (tf2::TransformException &ex) {
+      ROS_WARN("%s",ex.what());
+  }
+
+
+  // Add new data to viewer
+  //viewer.showCloud(cloudPtr);
+
+  // Publish the data.
+  pub.publish(cloud_out);
 }
 
 /** @brief Main code
@@ -150,12 +197,12 @@ int main(int argc, char** argv)
   tf2_ros::Buffer tfBuffer;
   tf2_ros::TransformListener tfListener(tfBuffer);
 
-  transformStamped = tfBuffer.lookupTransform("camera_color_left", "velo_link", ros::Time(0), ros::Duration(5.0));
+  transformStamped = tfBuffer.lookupTransform("camera_color_left", "velo_link", ros::Time(0), ros::Duration(10.0));
 
   // Ros subscriber for ros msg for Point Cloud
   //ros::Subscriber sub_pcl = nh.subscribe<sensor_msgs::PointCloud2>("velodyne_points", 1, callback_pcl);
   //sub_pcl = nh.subscribe<sensor_msgs::PointCloud2>("velodyne_points", 1, callback_pcl);
-  //pub = nh.advertise<sensor_msgs::PointCloud2>("output", 1);
+  pub = nh.advertise<sensor_msgs::PointCloud2>("output", 1);
 
   //cv::namedWindow("view");
   //image_transport::ImageTransport it(nh);
