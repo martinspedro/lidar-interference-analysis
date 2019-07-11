4583dd59c7edf34bff79f6c446754f28ccd9afdc
Pedro Martins <martinspedro@ua.pt>
2019-07-11 10:56:08 +0100

    Implementation of working point cloud visualizer

--- src/rigid_transform_computation/src/rigid_transform_computation.cpp
+++ src/rigid_transform_computation/src/rigid_transform_computation.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "rigid_transform_computation/imageVisualizer.hpp"
+#include "rigid_transform_computation/pointCloudVisualizer.hpp"
 
 
 int main(int argc, char* argv[])
@@ -9,7 +10,8 @@ int main(int argc, char* argv[])
     std::cout << "Node init succesfully" << std::endl;
 
 
-    ImageVisualizer image_visualizer_object;
+    //ImageVisualizer image_visualizer_object;
+    point_cloud::PointCloudVisualizer point_cloud_visualizer_object("/velodyne_points", "velo");
 
     ros::spin();
     return EXIT_SUCCESS;
