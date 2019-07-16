3c8600246248b380cbb4c5491d60074fc355b3c1
Pedro Martins <martinspedro@ua.pt>
2019-07-16 14:32:48 +0100

    Add code to create independent pcl visualizer node

---
+++ src/rigid_transform_computation/src/point_cloud_visualizer_node.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "rigid_transform_computation/pointCloudVisualizer.hpp"
+
+int main(int argc, char* argv[])
+{
+    //Initialize ROS
+    ros::init(argc, argv, "Point_Cloud_Visualizer_Node");
+    std::cout << "Node init succesfully" << std::endl;
+
+
+    point_cloud::PointCloudVisualizer point_cloud_visualizer_object("/velodyne_points", "pcl_viewer_pose", "velo");
+    //point_cloud_visualizer_object.registerPointPickingCallback(point_cloud::PointCloudVisualizer::SINGLE_POINT_MODE);
+
+
+    ros::spin();
+
+    return EXIT_SUCCESS;
+}
