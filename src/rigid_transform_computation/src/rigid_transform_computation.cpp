d699cf23389e55d703d6b2fab71329269edb16ce
Pedro Martins <martinspedro@ua.pt>
2019-07-22 14:00:06 +0100

    Update Rigid Transform Computation to read calibration file and use it as the camera_info

--- src/rigid_transform_computation/src/rigid_transform_computation.cpp
+++ src/rigid_transform_computation/src/rigid_transform_computation.cpp
@@ -7,6 +7,9 @@
 
 #include "rigid_transform_computation/Pixel.h"
 #include "rigid_transform_computation/PointXYZI.h"
+#include <yaml-cpp/yaml.h>  // yaml-cpp is installed as a shared library in usr/local/
+
+#include <string>
 
 using namespace point_cloud;
 
@@ -104,6 +107,60 @@ int main(int argc, char* argv[])
     ros::Subscriber image_sub = nh.subscribe<rigid_transform_computation::Pixel>            ("camera/clicked_pixel", 1, pixelCallback);
     ros::Subscriber point_cloud_sub = nh.subscribe<rigid_transform_computation::PointXYZI>  ("velo/picked_point", 1, pointCloudCallback);
     ros::Subscriber camera_info_sub = nh.subscribe<sensor_msgs::CameraInfo>                 ("camera/camera_info", 1, cameraInfoCallback);
+    /*
+    if (argc == 2) { //argv is the first argumwent, file parh for camera calbiration is the second
+    /* http://library.isr.ist.utl.pt/docs/roswiki/yaml_cpp.html
+    https://ossyaritoori.hatenablog.com/entry/2017/08/16/Read_yaml_file_with_yaml-cpp_in_ROS_C%2B%2B_/_
+    https://github.com/jbeder/yaml-cpp/wiki/Tutorial
+    https://stackoverflow.com/questions/50150387/parsing-yaml-file-using-yaml-cpp-error
+
+    // /
+        std::cout << "Loading Calibration file" << argv[1] << std::endl;
+
+        YAML::Node camera_info_yaml = YAML::LoadFile(argv[1]);
+        std::cout << "Open" << std::endl;
+        sensor_msgs::CameraInfo camera_info_msg;
+
+        std::vector<double> K, R, P;
+        camera_info_msg.width = camera_info_yaml["image_width"].as<uint32_t>();
+        std::cout << "width" << std::endl;
+        camera_info_msg.height = camera_info_yaml["image_height"].as<uint32_t>();
+        std::cout << "hiegth" << std::endl;
+
+        camera_info_msg.distortion_model = camera_info_yaml["distortion_model"].as<std::string>();
+        std::cout << "D model" << std::endl;
+        //camera_info_msg.D = camera_info_yaml["distortion_coefficients"].as<std::vector<double>>();
+        std::cout << "Model" << std::endl;
+
+        K = camera_info_yaml["camera_matrix"].as<std::vector<float>>();
+        std::cout << "K" << std::endl;
+        R = camera_info_yaml["rectification_matrix"].as<std::vector<double>>();
+        std::cout << "R" << std::endl;
+        P = camera_info_yaml["projection_matrix"].as<std::vector<double>>();
+        std::cout << "P" << std::endl;
+
+        // conversion
+        // Coversion from std::vector<double> to boost::array<double> is not implicit supported on K, R and P matrices
+        // because they have a fixed number of elements
+        boost::array<double, 9ul> kl;
+        std::memcpy(&kl[0], &K[0], sizeof(double)*9);
+        boost::array<double, 9ul> rl;
+        std::memcpy(&rl[0], &R[0], sizeof(double)*9);
+        boost::array<double, 12ul> pl;
+        std::memcpy(&pl[0], &P[0], sizeof(double)*12);
+        std::cout << "conversion" << std::endl;
+        // put value
+        camera_info_msg.K =  kl;
+        camera_info_msg.R =  rl;
+        camera_info_msg.P =  pl;
+        std::cout << "passed to vector" << std::endl;
+    }
+*/
+
+
+
+
+
 
 
 
