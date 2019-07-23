84fe7e84c0308ba22d8e0c617fe649b39bd1fa74
Pedro Martins <martinspedro@ua.pt>
2019-07-23 18:59:38 +0100

    Service to calculate rigid body transform using the selected correspondences between pixels and 3D points

---
+++ src/rigid_transform_computation/src/rigidBodyTransform_client.cpp
@@ -0,0 +1,28 @@
+#include "ros/ros.h"
+#include "rigid_transform_computation/computeRigidBodyTransform.h"
+#include <cstdlib>
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "computeRigidBodyTransform_client");
+  if (argc != 2)
+  {
+    ROS_INFO("Usage: rigidBodyTransform_client solvePnpType");
+    return EXIT_FAILURE;
+  }
+
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<rigid_transform_computation::computeRigidBodyTransform>("computeRigidBodyTransform");
+  rigid_transform_computation::computeRigidBodyTransform srv;
+
+  srv.request.solvePnpType = argv[1];
+
+  if (client.call(srv)) {
+    std::cout << srv.response.rigidBodyTransform << std::endl;
+  }
+  else {
+    ROS_ERROR("Failed to call service computeRigidBodyTransform");
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
--- src/rigid_transform_computation/src/rigid_transform_computation.cpp
+++ src/rigid_transform_computation/src/rigid_transform_computation.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+
 #include "rigid_transform_computation/imageVisualizer.hpp"
 #include "rigid_transform_computation/pointCloudVisualizer.hpp"
 
@@ -11,7 +13,13 @@
 
 #include <string>
 
+#include "rigid_transform_computation/computeRigidBodyTransform.h" //!< Srv message for rigid body transform computation
+#include "rigid_transform_computation/save_correspondences.h"   //!< Service to save correspondences
+#include "rigid_transform_computation/CSV_manager.hpp"
+
 using namespace point_cloud;
+
+const std::string THESIS_FILE_PATH = "/media/martinspedro/DATA/UA/Thesis/multiple-lidar-interference-mitigation/";
 
 typedef struct Pixel_struct {
     uint32_t x;
@@ -43,11 +51,100 @@ cv::Point3f transformLiDARToCameraReferential(cv::Point3f lidar_point) {
     temp3DPoint.z = lidar_point.x;
 }
 
+bool saveCorrespondencesSrvCallback(rigid_transform_computation::save_correspondences::Request  &req,
+                                      rigid_transform_computation::save_correspondences::Response &res) {
+
+        //std::cout << req.filename << std::endl;
+        //std::cout << imagePixels << std::endl;
+        //std::cout << pointCloudPoints << std::endl;
+        //std::cout << "Creating file" << std::endl;
+        std::ostringstream oss;
+
+        if (!( (imagePixels.size() == 0) | (pointCloudPoints.size() == 0) )) {
+            if (imagePixels.size() == pointCloudPoints.size()) {
+                create(THESIS_FILE_PATH + req.filename, imagePixels, pointCloudPoints);
+
+                oss << "File saved in " << req.filename;
+                res.message = oss.str();
+                res.success = true;
+            } else {
+                oss << "Number of selected pixels differs from the number of selected 3D Points: "<< imagePixels.size() << " vs " << pointCloudPoints.size();
+                res.message = oss.str();
+                res.success = false;
+            }
+        } else {
+            oss << "No pair of data points have yet been selecting. Aborting!";
+            res.message = oss.str();
+            res.success = false;
+        }
+
+        return true;    // Return true means the service is called
+}
+
+bool computeRigidTransformSrvCallback(rigid_transform_computation::computeRigidBodyTransform::Request  &req,
+                                      rigid_transform_computation::computeRigidBodyTransform::Response &res) {
+  cv::solvePnP(pointCloudPoints, imagePixels, camera_info[2].fullIntrinsicMatrix(), camera_info[2].distortionCoeffs(), rVec, tVec, false, cv::SOLVEPNP_ITERATIVE);
+
+  std::cout << "Rotation Vector: " << rVec << std::endl;
+  std::cout << "Translation Vector: " << tVec << std::endl;
+
+  cv::Mat rMat = cv::Mat(3, 3, CV_32FC1);
+  cv::Vec3f eulerVec;
+  cv::Rodrigues(rVec,rMat);
+  std::cout << "Rotation Matrix: " << rMat << std::endl;
+  cv::Mat projectionMatrix = cv::Mat(3, 4, CV_32FC1);
+  std::cout << "Projection Matrix: " << rMat << std::endl;
+  cv:hconcat(rMat, rVec.t(), projectionMatrix);
+
+  cv::Mat rotMatrixX,rotMatrixY,rotMatrixZ;
+  cv::Vec3f eulerAngles;
+
+  cv::decomposeProjectionMatrix( projectionMatrix,
+                                 camera_info[2].fullIntrinsicMatrix(),
+                                 rMat,
+                                 tVec,
+                                 rotMatrixX,
+                                 rotMatrixY,
+                                 rotMatrixZ,
+                                 eulerAngles);
+
+  geometry_msgs::Pose rigidBodyTransform;
+  return true;
+}
+
+
+
+
+
+void getEulerAngles(cv::Mat &rotCamerMatrix,cv::Vec3f &eulerAngles){
+    cv::Mat cameraMatrix,rotMatrix,transVect,rotMatrixX,rotMatrixY,rotMatrixZ;
+    float* _r = rotCamerMatrix.ptr<float>();
+    float projMatrix[12] = {_r[0],_r[1],_r[2],0,
+                          _r[3],_r[4],_r[5],0,
+                          _r[6],_r[7],_r[8],0};
+
+    cv::decomposeProjectionMatrix( cv::Mat(3,4,CV_32FC1,projMatrix),
+                               cameraMatrix,
+                               rotMatrix,
+                               transVect,
+                               rotMatrixX,
+                               rotMatrixY,
+                               rotMatrixZ,
+                               eulerAngles);
+}
+
 void computeRigidTransform(){
     //solvePnP(InputArray objectPoints, InputArray imagePoints, InputArray cameraMatrix, InputArray distCoeffs, OutputArray rvec, OutputArray tvec, bool useExtrinsicGuess=false, int flags=ITERATIVE )¶
     cv::solvePnP(pointCloudPoints, imagePixels, camera_info[10].fullIntrinsicMatrix(), camera_info[10].distortionCoeffs(), rVec, tVec, false, cv::SOLVEPNP_ITERATIVE);
     std::cout << rVec << std::endl;
     std::cout << tVec << std::endl;
+
+    cv::Mat rMat = cv::Mat_<float>(3, 3);
+    cv::Vec3f eulerVec;
+    cv::Rodrigues(rVec,rMat);
+    std::cout << rMat << std::endl;
+    getEulerAngles(rMat, eulerVec);
+    std::cout << eulerVec << std::endl;
 
     ros::shutdown(); // send shutdown command to node because its functionality its over
 
@@ -72,7 +169,7 @@ void pointCloudCallback(const rigid_transform_computation::PointXYZI::ConstPtr& 
     temp3DPoint.y = msg->y;
     temp3DPoint.z = msg->z;
 
-    pointCloudPoints.push_back(transformLiDARToCameraReferential(temp3DPoint));
+    pointCloudPoints.push_back(temp3DPoint);
 
     std::cout << "Added Point3D: " << temp3DPoint << std::endl;
 }
@@ -86,12 +183,13 @@ void cameraInfoCallback(const sensor_msgs::CameraInfoConstPtr& cam_info) {
     //std::cout << tempCameraModel.fullIntrinsicMatrix() << std::endl;
     //std::cout << "Added Camera Info " << std::endl;
     //std::cout << tempCameraModel.distortionCoeffs() << std::endl;
-
+    /*
     if ( (pointCloudPoints.size() >= 5) && (imagePixels.size() >= 5) )
     {
         std::cout << "Solving PnP" << std::endl;
         computeRigidTransform();
     }
+    */
 }
 
 
@@ -107,6 +205,9 @@ int main(int argc, char* argv[])
     ros::Subscriber image_sub = nh.subscribe<rigid_transform_computation::Pixel>            ("camera/clicked_pixel", 1, pixelCallback);
     ros::Subscriber point_cloud_sub = nh.subscribe<rigid_transform_computation::PointXYZI>  ("velo/picked_point", 1, pointCloudCallback);
     ros::Subscriber camera_info_sub = nh.subscribe<sensor_msgs::CameraInfo>                 ("camera/camera_info", 1, cameraInfoCallback);
+
+    ros::ServiceServer computationSrv = nh.advertiseService("computeRigidBodyTransform", computeRigidTransformSrvCallback);
+    ros::ServiceServer saveSrv = nh.advertiseService("save_correspondences", saveCorrespondencesSrvCallback);
     /*
     if (argc == 2) { //argv is the first argumwent, file parh for camera calbiration is the second
     /* http://library.isr.ist.utl.pt/docs/roswiki/yaml_cpp.html
