60bf3e254c25a5a32088b7fe92b8cdd8a32f4144
Pedro Martins <martinspedro@ua.pt>
2019-08-09 17:39:38 +0100

    Document save correspondences client node

--- src/rigid_transform_computation/src/save_correspondences_client.cpp
+++ src/rigid_transform_computation/src/save_correspondences_client.cpp
@@ -1,32 +1,53 @@
+/*!
+ *  \file   save_correspondences.cpp
+ *  \brief  Implementation of a service client node to request that the registed 2D <-> 3D correspondences be saved on a
+ * CSV file
+ *  \author Pedro Martins (martinspedro@av.it.pt)
+ *
+ */
+
 #include "ros/ros.h"
 #include "rigid_transform_computation/save_correspondences.h"
+
 #include <cstdlib>
 #include <string>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "save_correspondences_client");
-  if (argc != 2) {
-    ROS_INFO("Usage: save_corrrespondences filename");
+
+  // Client must be called by giving the output file name
+  if (argc != 2)
+  {
+    ROS_INFO("Usage: save_corrrespondences file name");
     return EXIT_FAILURE;
   }
 
-  ros::NodeHandle n;
-  ros::ServiceClient client = n.serviceClient<rigid_transform_computation::save_correspondences>("save_correspondences");
+  ros::NodeHandle nh;
+  // clang-format off
+  ros::ServiceClient client =
+        nh.serviceClient<rigid_transform_computation::save_correspondences>("save_correspondences");
+  // clang-format off
+
   rigid_transform_computation::save_correspondences srv;
   srv.request.filename = argv[1];
 
-  if (client.call(srv)) {
-      if(srv.response.success) {
-          std::cout << srv.response.message << std::endl;
-      }
-      else {
-          ROS_ERROR("%s", srv.response.message.c_str());
-      }
-
+  if (client.call(srv))
+  {
+    // service can be succesfully called but may not be able to save the file
+    if (srv.response.success)
+    {
+      ROS_INFO("%s", srv.response.message.c_str());
+    }
+    else
+    {
+      ROS_ERROR("%s", srv.response.message.c_str());
+    }
   }
-  else {
-      ROS_ERROR("Failed to call service save_correspondences");
-      return EXIT_FAILURE;
+  else
+  {
+    ROS_ERROR("Failed to call service save_correspondences");
+    return EXIT_FAILURE;
   }
 
   return EXIT_SUCCESS;
