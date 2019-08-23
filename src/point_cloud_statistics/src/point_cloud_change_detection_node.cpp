28a8f9f087dd036f61cdcf0b1083674e531d9826
Pedro Martins <martinspedro@ua.pt>
2019-08-23 17:41:51 +0100

    Ground Truth model estimation improvement using ICP
    Iteratively registers point cloud frames, aligns them and merges them
    Saves the model to a PCD file

--- src/point_cloud_statistics/src/point_cloud_change_detection_node.cpp
+++ src/point_cloud_statistics/src/point_cloud_change_detection_node.cpp
@@ -30,7 +30,196 @@
 #include <vtkWindowToImageFilter.h>
 #include <vtkPNGWriter.h>
 
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+#include <velodyne_pointcloud/point_types.h>
+
+#include <boost/make_shared.hpp>
+#include <pcl/point_representation.h>
+
+#include <pcl/io/pcd_io.h>
+
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/filter.h>
+
+#include <pcl/registration/icp.h>
+#include <pcl/registration/icp_nl.h>
+#include <pcl/registration/transforms.h>
+
+#include <pcl/visualization/pcl_visualizer.h>
+
+//#include "lz4.h"
+//#include "lz4hc.h"
+#include <pcl/features/normal_3d.h>
+typedef pcl::PointCloud<velodyne_pointcloud::PointXYZIR> VelodynePointCloud;
+
+using pcl::visualization::PointCloudColorHandlerCustom;
+using pcl::visualization::PointCloudColorHandlerGenericField;
+
+// convenient typedefs
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud<PointT> PointCloud;
+typedef pcl::PointNormal PointNormalT;
+typedef pcl::PointCloud<PointNormalT> PointCloudWithNormals;
+
+struct PCD
+{
+  PointCloud::Ptr cloud;
+  std::string f_name;
+
+  PCD() : cloud(new PointCloud){};
+};
+
+// Define a new point representation for < x, y, z, curvature >
+class MyPointRepresentation : public pcl::PointRepresentation<PointNormalT>
+{
+  using pcl::PointRepresentation<PointNormalT>::nr_dimensions_;
+
+public:
+  MyPointRepresentation()
+  {
+    // Define the number of dimensions
+    nr_dimensions_ = 4;
+  }
+
+  // Override the copyToFloatArray method to define our feature vector
+  virtual void copyToFloatArray(const PointNormalT& p, float* out) const
+  {
+    // < x, y, z, curvature >
+    out[0] = p.x;
+    out[1] = p.y;
+    out[2] = p.z;
+    out[3] = p.curvature;
+  }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+/** \brief Align a pair of PointCloud datasets and return the result
+ * \param cloud_src the source PointCloud
+ * \param cloud_tgt the target PointCloud
+ * \param output the resultant aligned source PointCloud
+ * \param final_transform the resultant transform between source and target
+ */
+void pairAlign(const PointCloud::Ptr cloud_src, const PointCloud::Ptr cloud_tgt, PointCloud::Ptr output,
+               Eigen::Matrix4f& final_transform, bool downsample = false)
+{
+  //
+  // Downsample for consistency and speed
+  // \note enable this for large datasets
+  PointCloud::Ptr src(new PointCloud);
+  PointCloud::Ptr tgt(new PointCloud);
+  pcl::VoxelGrid<PointT> grid;
+  if (downsample)
+  {
+    grid.setLeafSize(0.05, 0.05, 0.05);
+    grid.setInputCloud(cloud_src);
+    grid.filter(*src);
+
+    grid.setInputCloud(cloud_tgt);
+    grid.filter(*tgt);
+  }
+  else
+  {
+    src = cloud_src;
+    tgt = cloud_tgt;
+  }
+
+  // Compute surface normals and curvature
+  PointCloudWithNormals::Ptr points_with_normals_src(new PointCloudWithNormals);
+  PointCloudWithNormals::Ptr points_with_normals_tgt(new PointCloudWithNormals);
+
+  pcl::NormalEstimation<PointT, PointNormalT> norm_est;
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>());
+  norm_est.setSearchMethod(tree);
+  norm_est.setKSearch(30);
+
+  norm_est.setInputCloud(src);
+  norm_est.compute(*points_with_normals_src);
+  pcl::copyPointCloud(*src, *points_with_normals_src);
+
+  norm_est.setInputCloud(tgt);
+  norm_est.compute(*points_with_normals_tgt);
+  pcl::copyPointCloud(*tgt, *points_with_normals_tgt);
+
+  //
+  // Instantiate our custom point representation (defined above) ...
+  MyPointRepresentation point_representation;
+  // ... and weight the 'curvature' dimension so that it is balanced against x, y, and z
+  float alpha[4] = { 1.0, 1.0, 1.0, 1.0 };
+  point_representation.setRescaleValues(alpha);
+
+  //
+  // Align
+  pcl::IterativeClosestPointNonLinear<PointNormalT, PointNormalT> reg;
+  reg.setTransformationEpsilon(1e-6);
+  // Set the maximum distance between two correspondences (src<->tgt) to 10cm
+  // Note: adjust this based on the size of your datasets
+  reg.setMaxCorrespondenceDistance(0.1);
+  // Set the point representation
+  reg.setPointRepresentation(boost::make_shared<const MyPointRepresentation>(point_representation));
+
+  reg.setInputSource(points_with_normals_src);
+  reg.setInputTarget(points_with_normals_tgt);
+
+  //
+  // Run the same optimization in a loop and visualize the results
+  Eigen::Matrix4f Ti = Eigen::Matrix4f::Identity(), prev, targetToSource;
+  PointCloudWithNormals::Ptr reg_result = points_with_normals_src;
+  reg.setMaximumIterations(50);
+  reg.align(*reg_result);
+  Ti = reg.getFinalTransformation();
+  /*
+  for (int i = 0; i < 30; ++i)
+  {
+    PCL_INFO("Iteration Nr. %d.\n", i);
+
+    // save cloud for visualization purpose
+    points_with_normals_src = reg_result;
+
+    // Estimate
+    reg.setInputSource(points_with_normals_src);
+    reg.align(*reg_result);
+
+    // accumulate transformation between each Iteration
+    Ti = reg.getFinalTransformation() * Ti;
+
+    // if the difference between this transformation and the previous one
+    // is smaller than the threshold, refine the process by reducing
+    // the maximal correspondence distance
+    if (std::abs((reg.getLastIncrementalTransformation() - prev).sum()) < reg.getTransformationEpsilon())
+      reg.setMaxCorrespondenceDistance(reg.getMaxCorrespondenceDistance() - 0.001);
+
+    prev = reg.getLastIncrementalTransformation();
+
+    // visualize current state
+    // showCloudsRight(points_with_normals_tgt, points_with_normals_src);
+  }
+*/
+  //
+  // Get the transformation from target to source
+  targetToSource = Ti.inverse();
+
+  //
+  // Transform target back in source frame
+  pcl::transformPointCloud(*cloud_tgt, *output, targetToSource);
+
+  // p->removePointCloud("source");
+  // p->removePointCloud("target");
+
+  // PointCloudColorHandlerCustom<PointT> cloud_tgt_h(output, 0, 255, 0);
+  // PointCloudColorHandlerCustom<PointT> cloud_src_h(cloud_src, 255, 0, 0);
+  // p->addPointCloud(output, cloud_tgt_h, "target", vp_2);
+  // p->addPointCloud(cloud_src, cloud_src_h, "source", vp_2);
+
+  // PCL_INFO("Press q to continue the registration.\n");
+  // p->spin();
+
+  // p->removePointCloud("source");
+  // p->removePointCloud("target");
+
+  // add the source to the transformed target
+  *output += *cloud_src;
+
+  final_transform = targetToSource;
+}
 
 inline const std::string constructFullPathToDataset(const std::string dataset_name, const std::string file_name)
 {
@@ -123,6 +312,7 @@ int main(int argc, char** argv)
 
   PointCloud::Ptr current_msg_cloud_ptr(new PointCloud);
   PointCloud::Ptr ground_truth_ptr(new PointCloud);
+  // velodyne_pointcloud::PointcloudXYZIR::Ptr ground_truth_ptr(new velodyne_pointcloud::PointcloudXYZIR);
 
   rosbag::Bag interference_bag, ground_truth_bag;
   ground_truth_bag.open(ground_truth_full_bag_path);  // open ground truth bag file
@@ -131,18 +321,111 @@ int main(int argc, char** argv)
   topics.push_back(std::string("/velodyne_points"));
   rosbag::View ground_truth_view(ground_truth_bag, rosbag::TopicQuery(topics));
 
+  std::cout << "Ground Truth" << std::endl;
+  int count = 0;
+  long int sum = 0, sum_squared = 0;
   // generate ground truth model for test scenario
+
+  // Tutorial
+  PointCloud::Ptr result(new PointCloud), source(new PointCloud), target(new PointCloud);
+  Eigen::Matrix4f GlobalTransform = Eigen::Matrix4f::Identity(), pairTransform;
+
   foreach (rosbag::MessageInstance const m, ground_truth_view)
   {
     sensor_msgs::PointCloud2::ConstPtr msg = m.instantiate<sensor_msgs::PointCloud2>();
     if (msg != NULL)
     {
+      // VelodynePointCloud ground_truth_point_cloud;
       PointCloud ground_truth_point_cloud;
       fromROSMsg(*msg, ground_truth_point_cloud);
-      *ground_truth_ptr = ground_truth_point_cloud;
-      break;
+      //*ground_truth_ptr = ground_truth_point_cloud;
+
+      std::cout << "(" << msg->width << ", " << msg->height << ")" << std::endl;
+      sum += msg->width;
+      sum_squared += msg->width * msg->width;
+      /*
+      for (int i = 0; i < msg->fields.size(); ++i)
+      {
+        std::cout << "Field " << i << ": (" << msg->fields[i].name << ", " << msg->fields[i].offset << ", "
+                  << msg->fields[i].datatype << ", " << msg->fields[i].count << ")" << std::endl;
+      }
+      // std::cout << msg->fields << std::endl;
+      std::cout << "(" << ground_truth_point_cloud.width << ", " << ground_truth_point_cloud.height << ")" << std::endl;
+
+      std::cout << pcl::getFieldsList(ground_truth_point_cloud) << std::endl << std::endl;
+      */
+      *target = ground_truth_point_cloud;
+      // Tutorial starts here
+      if (count > 0)
+      {
+        // for (size_t i = 1; i < data.size(); ++i)
+        // {
+        // source = data[i - 1].cloud;
+        // target = data[i].cloud;
+
+        // Add visualization data
+        // showCloudsLeft(source, target);
+
+        PointCloud::Ptr temp(new PointCloud);
+        // PCL_INFO("Aligning %s (%d) with %s (%d).\n", data[i - 1].f_name.c_str(), source->points.size(),
+        //         data[i].f_name.c_str(), target->points.size());
+        pairAlign(source, target, temp, pairTransform, false);
+
+        // transform current pair into the global transform
+        pcl::transformPointCloud(*temp, *result, GlobalTransform);
+
+        // update the global transform
+        GlobalTransform *= pairTransform;
+
+        *source = *result;
+      }
+      else
+      {
+        *source = ground_truth_point_cloud;
+      }
+      /*
+      std::vector<int> field_sizes;
+      std::vector<sensor_msgs::PointField> point_fields;
+
+      pcl::getFields(ground_truth_point_cloud, point_fields);
+      pcl::getFieldsSizes(point_fields, field_sizes);
+
+      for (int i = 0; i < field_sizes.size(); ++i)
+      {
+        std::cout << field_sizes[i] << " ";
+      }
+
+      std::cout << std::endl;
+      */
+      /*
+      for (int i = 0; i < ground_truth_point_cloud.size(); ++i)
+      {
+        ground_truth_point_cloud.poins[i].
+      }
+      */
+      ++count;
+      std::cout << "Message number: " << count << std::endl;
+      if (count > 10)
+      {
+        break;
+      }
     }
   }
+  // save aligned pair, transformed into the first cloud's frame
+  std::stringstream ss;
+  ss << constructFullPathToDataset(argv[1], "ground_truth_model.pcd");
+  pcl::io::savePCDFile(ss.str(), *result, true);
+
+  //}
+  float average = (double)sum / count;
+  float variance = ((double)sum_squared - sum * sum) / count;
+  std::cout << "Sum: " << sum << std::endl;
+  std::cout << "Average: " << average << std::endl;
+  std::cout << "Variance: " << variance << std::endl;
+  std::cout << "Number of clouds: " << count << std::endl;
+  std::cout << result->size() << std::endl;
+  // return 0;
+  ground_truth_ptr = result;
 
   std::vector<double> ground_truth_errors, interference_errors, resolution_values;
 
