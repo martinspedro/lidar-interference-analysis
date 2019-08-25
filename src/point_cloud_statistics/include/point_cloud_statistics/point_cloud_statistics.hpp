83e00114d8869dc6fab22803faede0d517b514d4
Pedro Martins <martinspedro@ua.pt>
2019-08-25 14:56:54 +0100

    Move ground truth generation algorithms to point_cloud_statistics namespace files

--- src/point_cloud_statistics/include/point_cloud_statistics/point_cloud_statistics.hpp
+++ src/point_cloud_statistics/include/point_cloud_statistics/point_cloud_statistics.hpp
@@ -7,12 +7,19 @@
 #ifndef POINT_CLOUD_STATISTICS_H
 #define POINT_CLOUD_STATISTICS_H
 
-#include "point_cloud_statistics/point_cloud_statistics.hpp"
 #include <string>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud<PointT> PointCloud;
 
 namespace point_cloud_statistics
 {
 const std::string constructFullPathToDataset(const std::string dataset_name, const std::string file_name);
+
+void pairAlign(const PointCloud::Ptr cloud_src, const PointCloud::Ptr cloud_tgt, PointCloud::Ptr output,
+               Eigen::Matrix4f& final_transform, bool downsample);
 }  // namespace point_cloud_statistics
 
 #endif  // POINT_CLOUD_STATISTICS_H
