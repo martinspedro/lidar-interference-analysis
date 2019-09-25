e4ed92f3884daa5464fec030c597ec94501951b5
Pedro Martins <martinspedro@ua.pt>
2019-09-25 12:08:42 +0100

    Template class to create and manipulate Organized Point Cloud Objects

--- src/point_cloud_statistics/include/point_cloud_statistics/organized_pointcloud.hpp
+++ src/point_cloud_statistics/include/point_cloud_statistics/organized_pointcloud.hpp
@@ -4,40 +4,18 @@
  *
  */
 
-#ifndef ORGANIZED_POINTCLOUD_H
-#define ORGANIZED_POINTCLOUD_H
+#ifndef ORGANIZED_POINT_CLOUD_H
+#define ORGANIZED_POINT_CLOUD_H
 
-//#include <pcl/point_types.h>
+#include "point_cloud_statistics/organized_point_cloud_utilities.hpp"
 #include <pcl/point_cloud.h>
 
 #include <ros/ros.h>
-
-#define _USE_MATH_DEFINES  // Define to use PI
-#include <math.h>
+#include <ros/console.h>
 
 // namespace organized_pointcloud
 //{
-const float RADIAN_TO_DEGREE_F = 180.0F / (float)(M_PI);
-const double RADIAN_TO_DEGREE_D = 180.0D / (double)(M_PI);
-const long double RADIAN_TO_DEGREE_LD = 180.0L / (long double)(M_PI);
 
-const float DEGREE_OFFSET_TO_POSITIVE_ANGLE_F = 180.0F;
-const double DEGREE_OFFSET_TO_POSITIVE_ANGLE_D = 180.0D;
-const long double DEGREE_OFFSET_TO_POSITIVE_ANGLE_L = 180.0L;
-
-const float AZIMUTH_NORMALIZE_FACTOR_DEGREE_F = 360.0F;
-const double AZIMUTH_NORMALIZE_FACTOR_DEGREE_D = 360.0D;
-const long double AZIMUTH_NORMALIZE_FACTOR_DEGREE_L = 360.0L;
-/*
-float getAzimuth(float y, float x);
-double getAzimuth(double y, double x);
-long double getAzimuth(long double y, long double x);
-
-float computeEuclideanDistance(float x_1, float y_1, float z_1, float x_2, float y_2, float z_2);
-double computeEuclideanDistance(double x_1, double y_1, double z_1, double x_2, double y_2, double z_2);
-long double computeEuclideanDistance(long double x_1, long double y_1, long double z_1, long double x_2,
-                                     long double y_2, long double z_2);
-*/
 template <class T>
 class OrganizedPointCloud : public pcl::PointCloud<T>
 {
@@ -64,12 +42,14 @@ float computeEuclideanDistance(T point1, T point2);
   // template <class T>
   OrganizedPointCloud(unsigned int width, unsigned int height) : pcl::PointCloud<T>(width, height)
   {
-    this->is_dense = false;  // Organized Point Cloud is not dense
+    ROS_DEBUG_NAMED("call_stack", "[OrganizedPointCloud] with (%d, %d)", width, height);
   }
 
   // template <typename T>
   void clearPointsFromPointcloud()
   {
+    ROS_DEBUG_NAMED("call_stack", "[clearPointsFromPointcloud]");
+
     T aux;
     aux.x = std::numeric_limits<float>::quiet_NaN();
     aux.y = std::numeric_limits<float>::quiet_NaN();
@@ -89,15 +69,19 @@ float computeEuclideanDistance(T point1, T point2);
   // template <typename T>
   float getAzimuth(const T point)
   {
-    return atan2(point.y, point.x) * RADIAN_TO_DEGREE_F;
+    ROS_DEBUG_NAMED("call_stack", "[getAzimuth]");
+    return atan2(point.y, point.x) * point_cloud::organized::RADIAN_TO_DEGREE_F;
   }
 
   // template <typename T>
   unsigned int getAzimuthIndex(const T point)
   {
-    float shifted_azimuth = OrganizedPointCloud<T>::getAzimuth(point) + DEGREE_OFFSET_TO_POSITIVE_ANGLE_F;
+    ROS_DEBUG_NAMED("call_stack", "[getAzimuthIndex]");
+    float shifted_azimuth =
+        OrganizedPointCloud<T>::getAzimuth(point) + point_cloud::organized::DEGREE_OFFSET_TO_POSITIVE_ANGLE_F;
     float fixed_point_shifted_azimuth = floor(shifted_azimuth * 10.0f) / 10.0f;
-    float index = fixed_point_shifted_azimuth * (float)(this->width) / AZIMUTH_NORMALIZE_FACTOR_DEGREE_F;
+    float index =
+        fixed_point_shifted_azimuth * (float)(this->width - 1) / point_cloud::organized::FULL_REVOLUTION_DEGREE_F;
 
     return (unsigned int)(index);
   }
@@ -105,12 +89,15 @@ float computeEuclideanDistance(T point1, T point2);
   // template <typename T>
   float computeEuclideanDistanceToOrigin(T point)
   {
+    ROS_DEBUG_NAMED("call_stack", "[computeEuclideanDistanceToOrigin]");
     return std::sqrt(point.x * point.x + point.y * point.y + point.z * point.z);
   }
 
   // template <typename T>
   float computeEuclideanDistance(T point1, T point2)
   {
+    ROS_DEBUG_NAMED("call_stack", "[computeEuclideanDistance]");
+    ROS_INFO("P1->(%f, %f, %f), P2->(%f, %f, %f)", point1.x, point1.y, point1.z, point2.x, point2.y, point2.z);
     float x_diff = point2.x - point1.x;
     float y_diff = point2.y - point1.y;
     float z_diff = point2.z - point1.z;
@@ -121,7 +108,10 @@ float computeEuclideanDistance(T point1, T point2);
   // template <typename T>
   void organizeVelodynePointCloud(pcl::PointCloud<T> unorganized_cloud)
   {
+    ROS_DEBUG_NAMED("call_stack", "[organizeVelodynePointCloud]");
     this->header = unorganized_cloud.header;  // use sequence number, stamp and frame_id from the unorganized cloud
+    this->is_dense = false;                   // Organized Point Cloud is not dense
+
     T aux;
     aux.x = std::numeric_limits<float>::quiet_NaN();
     aux.y = std::numeric_limits<float>::quiet_NaN();
@@ -131,20 +121,22 @@ float computeEuclideanDistance(T point1, T point2);
 
     for (int i = 0; i < unorganized_cloud.size(); ++i)
     {
-      unsigned int azimuth = getAzimuthIndex(unorganized_cloud.points[i]);
-      // (width, height)
-      if (this->at(azimuth, unorganized_cloud.points[i].ring).intensity != std::numeric_limits<float>::quiet_NaN())
+      unsigned int azimuth_index = getAzimuthIndex(unorganized_cloud.points[i]);
+
+      ROS_ASSERT_MSG(azimuth_index < this->width, "Azimuth index %d vs size %d", azimuth_index, this->width);
+
+      if (this->at(azimuth_index, unorganized_cloud.points[i].ring).intensity !=
+          std::numeric_limits<float>::quiet_NaN())
       {
-        this->at(azimuth, unorganized_cloud.points[i].ring) = unorganized_cloud.points[i];
+        this->at(azimuth_index, unorganized_cloud.points[i].ring) = unorganized_cloud.points[i];
       }
       else
       {
-        /*
-      ROS_WARN("Point would erase previous point! (%f, %f, %f) vs (%f, %f, %f) and azimuth: %f. Point not added!",
-               this->at(azimuth, unorganized_cloud.points[i].ring).x,
-               this->at(azimuth, unorganized_cloud.points[i].ring).y, unorganized_cloud.points[i].ring,
-               unorganized_cloud.points[i].x, unorganized_cloud.points[i].y, azimuth);
-               */
+        ROS_WARN("Point would erase previous point! (%f, %f, %f) vs (%f, %f, %f) and azimuth_index: %d. Point not "
+                 "added!",
+                 this->at(azimuth_index, unorganized_cloud.points[i].ring).x,
+                 this->at(azimuth_index, unorganized_cloud.points[i].ring).y, unorganized_cloud.points[i].ring,
+                 unorganized_cloud.points[i].x, unorganized_cloud.points[i].y, azimuth_index);
       }
     }
   }
@@ -153,6 +145,8 @@ float computeEuclideanDistance(T point1, T point2);
   OrganizedPointCloud<T> computeDistanceBetweenPointClouds(OrganizedPointCloud<T> point_cloud_1,
                                                            OrganizedPointCloud<T> point_cloud_2)
   {
+    ROS_DEBUG_NAMED("call_stack", "[computeDistanceBetweenPointClouds]");
+
     ROS_ASSERT_MSG((point_cloud_1.width == point_cloud_2.width) && (point_cloud_1.height == point_cloud_2.height),
                    "Point cloud dimensions disagree: (%i, %i) vs (%i, %i)", point_cloud_1.width, point_cloud_1.height,
                    point_cloud_2.width, point_cloud_2.height);
@@ -171,8 +165,10 @@ float computeEuclideanDistance(T point1, T point2);
   }
 
   // template <typename T>
-  void computeDistanceBetweenPointClouds(OrganizedPointCloud<T> point_cloud, std::vector<float> distance_vector)
+  void computeDistanceBetweenPointClouds(OrganizedPointCloud<T> point_cloud, std::vector<double>& distance_vector)
   {
+    ROS_DEBUG_NAMED("call_stack", "[computeDistanceBetweenPointClouds]");
+    ROS_INFO("(%i, %i) vs (%i, %i)", this->width, this->height, point_cloud.width, point_cloud.height);
     ROS_ASSERT_MSG((this->width == point_cloud.width) && (this->height == point_cloud.height),
                    "Point cloud dimensions disagree: (%i, %i) vs (%i, %i)", this->width, this->height,
                    point_cloud.width, point_cloud.height);
@@ -181,11 +177,13 @@ float computeEuclideanDistance(T point1, T point2);
     {
       for (int j = 0; j < this->height; ++j)
       {
-        distance_vector.push_back(computeEuclideanDistance(this->at(i, j), point_cloud.at(i, j)));
+        double distance = (double)(computeEuclideanDistance(this->at(i, j), point_cloud.at(i, j)));
+        ROS_INFO("Distance: %f", distance);
+        distance_vector.push_back(distance);
       }
     }
   }
 };
 //}  // namespace organized_pointcloud
 
-#endif  // ORGANIZED_POINTCLOUD_H
+#endif  // ORGANIZED_POINT_CLOUD_H
