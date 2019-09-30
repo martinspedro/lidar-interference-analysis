ce6671a0962d05d6d6b574b6dd1cb130b327f431
Pedro Martins <martinspedro@ua.pt>
2019-09-30 17:26:09 +0100

    VLP16 constant files

---
+++ src/point_cloud_statistics/include/point_cloud_statistics/vlp_16_utilities.hpp
@@ -0,0 +1,18 @@
+/**
+ * \file   vlp_16_utilities.hpp
+ * \brief Constants used for VLP16
+ *
+ */
+
+#ifndef VLP_16_UTILITIES_H
+#define VLP_16_UTILITIES_H
+
+namespace vlp16
+{
+extern const unsigned int VLP16_LASER_COUNT;
+extern const float AZIMUTHAL_ANGULAR_RESOLUTION;
+extern const unsigned int AZIMUTHAL_UNIQUE_ANGLES_COUNT;
+
+}  // namespace vlp16
+
+#endif  // VLP_16_UTILITIES_H
---
+++ src/point_cloud_statistics/src/vlp_16_utilities.cpp
@@ -0,0 +1,17 @@
+/**
+ * \file   vlp_16_utilities.cpp
+ * \brief Constants used for VLP16
+ *
+ */
+
+#include "point_cloud_statistics/vlp_16_utilities.hpp"
+#include "point_cloud_statistics/organized_point_cloud_utilities.hpp"
+
+namespace vlp16
+{
+const unsigned int VLP16_LASER_COUNT = 16u;
+const float AZIMUTHAL_ANGULAR_RESOLUTION = 0.2F;
+const unsigned int AZIMUTHAL_UNIQUE_ANGLES_COUNT =
+    (unsigned int)ceil(point_cloud::organized::FULL_REVOLUTION_DEGREE_F / AZIMUTHAL_ANGULAR_RESOLUTION);
+
+}  // namespace vlp16
