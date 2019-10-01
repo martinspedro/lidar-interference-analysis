c7f0ecc02cc4c9fe524e4bce5b6c4846917a5d93
Pedro Martins <martinspedro@ua.pt>
2019-10-01 14:46:27 +0100

    Add velodyne namespace to point definition and typedef Velodyne Point Cloud

--- src/point_cloud_statistics/include/point_cloud_statistics/vlp_16_utilities.hpp
+++ src/point_cloud_statistics/include/point_cloud_statistics/vlp_16_utilities.hpp
@@ -7,6 +7,13 @@
 #ifndef VLP_16_UTILITIES_H
 #define VLP_16_UTILITIES_H
 
+#include <point_cloud_statistics/velodyne_point_type.h>
+#include <pcl/point_cloud.h>
+
+namespace velodyne
+{
+typedef pcl::PointCloud<velodyne::PointXYZIR> VelodynePointCloud;
+
 namespace vlp16
 {
 extern const unsigned int VLP16_LASER_COUNT;
@@ -14,5 +21,7 @@ extern const float AZIMUTHAL_ANGULAR_RESOLUTION;
 extern const unsigned int AZIMUTHAL_UNIQUE_ANGLES_COUNT;
 
 }  // namespace vlp16
+
+}  // namespace velodyne
 
 #endif  // VLP_16_UTILITIES_H
--- src/point_cloud_statistics/src/vlp_16_utilities.cpp
+++ src/point_cloud_statistics/src/vlp_16_utilities.cpp
@@ -7,6 +7,8 @@
 #include "point_cloud_statistics/vlp_16_utilities.hpp"
 #include "point_cloud_statistics/organized_point_cloud_utilities.hpp"
 
+namespace velodyne
+{
 namespace vlp16
 {
 const unsigned int VLP16_LASER_COUNT = 16u;
@@ -15,3 +17,5 @@ const unsigned int AZIMUTHAL_UNIQUE_ANGLES_COUNT =
     (unsigned int)ceil(point_cloud::organized::FULL_REVOLUTION_DEGREE_F / AZIMUTHAL_ANGULAR_RESOLUTION);
 
 }  // namespace vlp16
+
+}  // namespace velodyne
