e29d25407785c53ccd49540c4627743691fc8fdf
Pedro Martins <martinspedro@ua.pt>
2019-10-05 15:44:14 +0100

    Increase data output on log routines

--- src/point_cloud_statistics/src/cloud_statistical_data.cpp
+++ src/point_cloud_statistics/src/cloud_statistical_data.cpp
@@ -37,7 +37,9 @@ void CloudStatisticalData::printStatistics()
   std::cout << "Number of received Point Cloud Messages: " << this->point_cloud_msg_count << std::endl
             << "Number of received Point Cloud 3D Points: " << this->point_count << std::endl
             << "From which " << this->outliers_points_count << " (" << this->getOutliersPercentage()
-            << "%) are interfered" << std::endl;
+            << "%) are interfered" << std::endl
+            << "with an Average Points per message of: "
+            << (double)(this->point_count) / (double)(this->point_cloud_msg_count) << std::endl;
 }
 
 std::stringstream CloudStatisticalData::outputStringFormattedPointStatistics()
@@ -56,7 +58,9 @@ std::stringstream CloudStatisticalData::outputStringFormattedStatistics()
   temp_sstr << "Number of received Point Cloud Messages: " << this->point_cloud_msg_count << std::endl
             << "Number of received Point Cloud 3D Points: " << this->point_count << std::endl
             << "From which " << this->outliers_points_count << " (" << this->getOutliersPercentage()
-            << "%) are interfered" << std::endl;
+            << "%) are interfered" << std::endl
+            << "with an Average Points per message of: "
+            << (double)(this->point_count) / (double)(this->point_cloud_msg_count) << std::endl;
   return temp_sstr;
 }
 
