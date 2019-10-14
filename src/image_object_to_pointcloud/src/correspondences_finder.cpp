dff4ba450c99d111aecac2ed9f383376e4f43fc4
Pedro Martins <martinspedro@ua.pt>
2019-10-14 12:03:55 +0100

    Code Cleaning and formatting

--- src/image_object_to_pointcloud/src/correspondences_finder.cpp
+++ src/image_object_to_pointcloud/src/correspondences_finder.cpp
@@ -62,58 +62,23 @@
 #include <list>
 
 ros::Publisher pub;  //!< ROS Publisher
-
-std::list<int> g_number_of_occurrences;
-bool stuck = false;
-
-void number_of_objects_callback(const std_msgs::Int8ConstPtr& number_of_occurrences)
-{
-  if (stuck)  // this callback as run twice, but not the other
-  {
-    g_number_of_occurrences.clear();
-  }
-
-  std::cout << "Callback - Number of number_of_occurrences" << std::endl;
-  std::cout << "Number of occurrences" << (int)(number_of_occurrences->data) << std::endl;
-  g_number_of_occurrences.push_back((int)(number_of_occurrences->data));
-  stuck = true;
-}
-
-void callback(const darknet_ros_msgs::ObjectCountConstPtr& object_count, const darknet_ros_msgs::BoundingBoxesConstPtr& b_boxes, const sensor_msgs::CameraInfoConstPtr& cam_info,
+void callback(const darknet_ros_msgs::ObjectCountConstPtr& object_count,
+              const darknet_ros_msgs::BoundingBoxesConstPtr& b_boxes, const sensor_msgs::CameraInfoConstPtr& cam_info,
               const sensor_msgs::PointCloud2::ConstPtr& point_cloud_msg)
 {
   std::cout << "Callback" << std::endl;
-  /*
-  if (!g_number_of_occurrences.empty())
-  {
-    std::cout << "Callback with number of occurrences: " << g_number_of_occurrences.front() << std::endl;
-    std::list<int>::iterator it;
-    for (it = g_number_of_occurrences.begin(); it != g_number_of_occurrences.end(); it++)
-    {
-      std::cout << *it << " ";
-    }
 
-    std::cout << std::endl;
-*/
-    for (int i = 0; i < object_count->count; ++i)
-    {
-      try
-      {
-        std::cout << b_boxes->bounding_boxes[i].Class << ": " << b_boxes->bounding_boxes[i].probability << std::endl;
-      }
-      catch (std::exception& e)
-      {
-        std::cout << "Catched " << e.what() << std::endl;
-      }
-    }
-    /* g_number_of_occurrences.pop_front();  // remove the first element
-  }
-  else
+  for (int i = 0; i < object_count->count; ++i)
   {
-    std::cout << "List is empty" << std::endl;
+    try
+    {
+      std::cout << b_boxes->bounding_boxes[i].Class << ": " << b_boxes->bounding_boxes[i].probability << std::endl;
+    }
+    catch (std::exception& e)
+    {
+      std::cout << "Catched " << e.what() << std::endl;
+    }
   }
-  stuck = false;
-  */
 }
 
 int main(int argc, char** argv)
@@ -134,20 +99,19 @@ int main(int argc, char** argv)
 
   pub = nh.advertise<sensor_msgs::PointCloud2>("filtered_point_cloud", 1);
 
-  //ros::Subscriber detected_objects_sub = nh.subscribe("/darknet_ros/found_object", 10, number_of_objects_callback);
-
   message_filters::Subscriber<darknet_ros_msgs::ObjectCount> object_count_sub(nh, "/darknet_ros/found_object", 1);
   message_filters::Subscriber<darknet_ros_msgs::BoundingBoxes> bounding_boxes_sub(nh, "/darknet_ros/bounding_boxes", 1);
   message_filters::Subscriber<sensor_msgs::CameraInfo> info_sub(nh, "/kitti/camera_color_left/camera_info", 1);
   message_filters::Subscriber<sensor_msgs::PointCloud2> point_cloud_sub(nh, "/velodyne_points", 1);
 
-  typedef message_filters::sync_policies::ApproximateTime<darknet_ros_msgs::ObjectCount, darknet_ros_msgs::BoundingBoxes, sensor_msgs::CameraInfo,
-                                                          sensor_msgs::PointCloud2>
+  typedef message_filters::sync_policies::ApproximateTime<
+      darknet_ros_msgs::ObjectCount, darknet_ros_msgs::BoundingBoxes, sensor_msgs::CameraInfo, sensor_msgs::PointCloud2>
       MySyncPolicy;
-  // ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
-  message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), object_count_sub, bounding_boxes_sub, info_sub, point_cloud_sub);
 
-  // TimeSynchronizer<Image, sensor_msgs::PointCloud2> sync(image_sub, point_cloud_sub,  100);
+  // ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
+  message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), object_count_sub, bounding_boxes_sub, info_sub,
+                                                   point_cloud_sub);
+
   sync.registerCallback(boost::bind(&callback, _1, _2, _3, _4));
 
   ros::spin();
