107ef9e16496c4ae6481035771b4d7812a907e63
Pedro Martins <martinspedro@ua.pt>
2019-11-23 18:48:33 +0000

    Scripts to automatize the generation of box filter graphs

---
+++ src/graphics_generation/scripts/graphics_generation_box_filter_all_datasets.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rosrun graphics_generation box_filter_it2_dark_room_setups.py
+rosrun graphics_generation box_filter_distance_setups.py
+rosrun graphics_generation box_filter_direction_setups.py
+rosrun graphics_generation box_filter_height_setups.py
+rosrun graphics_generation box_filter_human_setups.py
+rosrun graphics_generation box_filter_LOS_setups.py
