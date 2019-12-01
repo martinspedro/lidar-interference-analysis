6b432b7b4283930033513819dd06832f0782ed0b
Pedro Martins <martinspedro@ua.pt>
2019-12-01 12:03:59 +0000

    Update script

--- src/point_cloud_statistics/scripts/point_cloud_change_detection_all_datasets.bash
+++ src/point_cloud_statistics/scripts/point_cloud_change_detection_all_datasets.bash
@@ -62,5 +62,5 @@ max_voxel_resolution=1.5
 voxel_resolution_step=0.05
 
 for i in ${test_scenarios[@]}; do
-  rosrun point_cloud_statistics point_cloud_change_detection_node $i $min_voxel_resolution $max_voxel_resolution  $voxel_resolution_step
+  rosrun point_cloud_statistics point_cloud_change_detection_node $i $min_voxel_resolution $max_voxel_resolution $voxel_resolution_step
 done
